e0a304599a4790fe39e983f09e2af484d2fc8ca0
Fernando Contreras <fercreek@gmail.com>
2016-04-25 02:45:38 -0500

    Show and hide articles

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 $(document).ready(function() {
 	$(".dropdown-button").dropdown();
-
 	$(".button-collapse").sideNav();
 	$('.collapsible').collapsible();
 	$('.slider').slider({full_width: true});
 	$('.carousel').carousel();
+
+	$('#vol1').hide(1000);
+	$('#vol2').hide(1000);
+
+	$('.volumeBtn').click(function () {
+		 var val = ($(this).attr("value"));
+		 if (val == 1) {
+		 	$('#vol1').show(1000);
+		 	$('#vol2').hide(1000);
+		 } else {
+		 	$('#vol1').hide(1000);
+		 	$('#vol2').show(1000);
+		 }
+
+	})
 
 });
 
